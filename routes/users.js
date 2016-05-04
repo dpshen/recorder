171ec05c02c50e50f10b861ada7f193cc797d8ec
shendapeng <shendapeng@yuantutech.com>
2016-05-04 18:21:24 +0800

    “dp”

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/signIn', function (req, res, next) {
+router.get('/signin', function (req, res, next) {
 
-    console.log(req.originalUrl);
+    console.log(req.originalurl);
     var data = req.query;
-    data.userId = 1;
-    data.userName = req.query.name;
+    data.userid = 1;
+    data.username = req.query.name;
     data.phone = req.query.phone;
 
     data.ticket = "57284d87719cc7d20da62fc8";
-    req.rst.setResult(data);
-    res.end(req.rst.toString())
+    req.rst.setresult(data);
+    res.end(req.rst.tostring())
+
+});
+
+router.get('/checkAuth', function (req, res, next) {
+
+    console.log(req.originalurl);
+    var data = req.query;
+    data.userid = 1;
+    data.ticket = "57284d87719cc7d20da62fc8";
+
+    req.rst.setresult(data);
+    res.end(req.rst.tostring())
 
 });
 
